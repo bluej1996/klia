c27cb1ef0b5276e8668f186d2ef66451c38f6e06
bluej1996 <bluej1996@gamil.com>
2022-05-10 15:41:33 +0900

    모달적용

--- js/script.js
+++ js/script.js
@@ -7,7 +7,21 @@ window.onload = function () {
             return window.innerWidth < desktop;
         }
     });
+    let modal_close = $('.modal-close');
+    let modal_close_2 = $('.modal-close-2');
+    let modal = $('.modal');
+    let modal_review = $('.modal-review');
+    let modal_review_bt = $('.modal-review-bt');
 
+    modal_close.click(function(){
+        modal.hide();
+    });
+    modal_close_2.click(function(){
+        modal.hide();
+    });
+    modal_review_bt.click(function(){
+        modal_review.show();
+    });
     // 스크롤시 애니메이션 
     $(".customer-box-cont").each(function (index, el) {
         new Waypoint({
